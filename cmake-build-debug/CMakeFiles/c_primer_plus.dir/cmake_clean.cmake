5942c0bcc7f1659de543fd03a0444eeebf02b3bd
cyw <18604469853@163.com>
2020-08-23 20:23:41 +0800

    default

---
+++ cmake-build-debug/CMakeFiles/c_primer_plus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_primer_plus.dir/main.c.o"
+  "c_primer_plus"
+  "c_primer_plus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_primer_plus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
